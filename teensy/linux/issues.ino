5e3f1bb91fbb7cd6b0eec6a7e2ab2ab786f2788f
Cyboot <Cyboot@timweb.de>
2013-06-30 13:18:06 +0200

    teensy

--- teensy/linux/issues.ino
+++ teensy/linux/issues.ino
@@ -2,14 +2,13 @@
 Know Issues/ToDos/not yet implemented Features:
 * 'cd ..' für übergeordnetes Directory
 * cd in ungültiges/nichtexistierendes Verzeichnis verhindern
-* man-pages für bekannte befehle
 
 BUGs:
 * rm löscht manche files nicht (z.b. /test/file1.txt)
 
 
 
-Big features (evtl. zu groß um sie zu implentieren)
+Big features
 * Tab-vervollständigung
 * mini Text-Editor zum Schreiben in Dateien
 
